1d3f5b57690655251fb8924b8142c7b4a9f33b37
ee11cbb19052e40b07aac0ca060c23ee <ee11cbb19052e40b07aac0ca060c23@gmail.com>
2017-03-29 13:48:09 -0400

    resolves #10
    * [build] Added build scripts to support publishing to Bintray and OJO

---
+++ gradle/snapshot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo '[snapshot] TRAVIS_BRANCH='$TRAVIS_BRANCH
+echo '[snapshot] TRAVIS_PULL_REQUEST='$TRAVIS_PULL_REQUEST
+
+# we only publish for specific branches and if NOT a PR
+if [[ "$TRAVIS_BRANCH" == "development" ]] && [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+    ./gradlew artifactoryPublish --stacktrace
+    exit $?
+fi
